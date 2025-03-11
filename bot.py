10bec855467175f059e1f4693fab86f1613f617b
rakesh-123-source <radheshyaam.gazol.1111@gmail.com>
2025-03-11 21:20:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ app = Flask('')
 def home():
     return "Bot is running!"
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080) 
 def keep_alive():
     t = Thread(target=run)
     t.start()
